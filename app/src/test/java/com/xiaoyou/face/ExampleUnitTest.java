f7214c6fbba31c575df2820d96b349d6f10d0936
limingkang <1151475385@qq.com>
2020-12-16 23:13:47 +0800

    Service完成

--- app/src/test/java/com/xiaoyou/face/ExampleUnitTest.java
+++ app/src/test/java/com/xiaoyou/face/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.xiaoyou.face;
 
+import com.xiaoyou.face.service.StudentInfo;
 import com.xiaoyou.face.utils.FaceRead;
 
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +23,9 @@ public class ExampleUnitTest {
 
     @Test
     public void faceTest() throws Exception {
-        FaceRead.checkFace("D:\\tmp\\2.jpg");
+        //FaceRead.checkFace("D:\\tmp\\2.jpg");
+        StudentInfo studentInfo = new StudentInfo();
+        studentInfo.setDateTime(LocalDate.now());
+        System.out.println(studentInfo.getDateTime().getMonthValue());
     }
 }
